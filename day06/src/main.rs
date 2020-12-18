eee69273a0f6fe321e9e83556df70cd020a449a5
Samuel Gosselin <gosselin.sam@gmail.com>
2020-12-17 21:49:46 -0800

    day06: add solution to part 2.

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use std::collections::HashMap;
 use std::env;
 use std::io::BufReader;
 use std::io::prelude::*;
@@ -36,6 +37,42 @@ fn count_num_questions(path: &str) -> usize {
     return res;
 }
 
+fn update_frequency_map(s: &str, count: &mut HashMap<char, usize>) {
+    for c in s.chars() {
+        let it = count.entry(c).or_insert(0);
+        *it += 1;
+    }
+}
+
+fn count_num_questions_part2(path: &str) -> usize {
+    let f = File::open(path)
+        .expect("could not open the input file");
+
+    let mut res = 0;
+    let mut freqs:HashMap<char, usize> = HashMap::new();
+    let mut num_groups = 0;
+
+    for data in BufReader::new(f).lines() {
+        let line = data
+            .expect("could not read line from input");
+        if line != "" {
+            update_frequency_map(&line, &mut freqs);
+            num_groups += 1;
+        } else {
+            // Count the number of answers that showed up in each group.
+            for (_, num) in &freqs {
+                if *num == num_groups {
+                    res += 1
+                }
+            }
+            // Reset the counters.
+            freqs.clear();
+            num_groups = 0;
+        }
+    }
+
+    res
+}
 fn main() {
     let args:Vec<String> = env::args()
         .collect();
@@ -44,6 +81,8 @@ fn main() {
         return;
     }
 
-    let res = count_num_questions(&args[1]);
-    println!("res: {}", res);
+    let res_part1 = count_num_questions(&args[1]);
+    println!("res (part 1): {}", res_part1);
+    let res_part2 = count_num_questions_part2(&args[1]);
+    println!("res (part 2): {}", res_part2);
 }
